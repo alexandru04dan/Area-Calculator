1babba6fcdc56dbe964adb1948fd770b1b4da4e8
Alexandru <alexandrudan04@outlook.com>
2024-12-19 12:13:45 +0200

    added possibility to calc square area

--- main.cpp
+++ main.cpp
@@ -6,15 +6,20 @@ namespace Shapes {
     double radius;
     double length;
     double width;
+    double side;
 
     double circleArea = 0;
     double rectangleArea = 0;
+    double squareArea = 0;
 
     void getCircleArea(){
         circleArea = Pi * pow(radius , 2);
     }
     void getRectangleArea(){
         rectangleArea = length * width;
+    }
+    void getSquareArea(){
+        squareArea = pow(side, 2);
     }
 }
 
@@ -26,6 +31,7 @@ int main(){
     std::cout << "What shape are you trying to calculate its area?" << '\n';
     std::cout << "a. Circle" << '\n';
     std::cout << "b. Rectangle" << '\n';
+    std::cout << "c. Square" << '\n';
     std::cin >> choice;
 
     switch(choice){
@@ -49,6 +55,21 @@ int main(){
             getRectangleArea();
 
             std::cout << "The rectangle area is: " << rectangleArea << '\n';
+
+            break;
+
+        case 'c':
+            std::cout << "Please type the value of the side of the sqaure:" << '\n';
+            std::cin >> side;
+
+            getSquareArea();
+
+            std::cout << "The square area is: " << squareArea << '\n';
+
+            break;
+
+        default:
+            std::cout << "Please enter one of the above!" << '\n';
 
             break;
     }
